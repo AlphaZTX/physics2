7e04b3dff6fcefa6382f5fe81a3ec51f291bbc4a
AlphaZTX <71686644+AlphaZTX@users.noreply.github.com>
2024-12-11 16:53:36 +0800

    Update zip.sh

--- zip.sh
+++ zip.sh
@@ -1,15 +1,17 @@
 #!/bin/zsh
 # For macOS X:
-mkdir physics2
-cp -r README.md physics2
-cp -r physics2-code.dtx physics2
-cp -r physics2-code.src.*.dtx physics2
-cp -r physics2.ins physics2
-cp -r physics2-code.pdf physics2
-cp -r doc/physics2.tex physics2
-cp -r doc/physics2-legacy.tex physics2
-cp -r doc/physics2.pdf physics2
-cp -r doc/physics2-legacy.pdf physics2
-cp -r doc/phy2docdef.tex physics2
-tar --exclude ".DS_Store" --exclude "__MACOSX" -cvzf "physics2.tar.gz" "physics2"
+if [ ! -d "physics2" ]; then
+  mkdir physics2
+fi
+cp README.md physics2
+cp physics2-code.dtx physics2
+cp physics2-code.src.*.dtx physics2
+cp physics2.ins physics2
+cp physics2-code.pdf physics2
+cp doc/physics2.tex physics2
+cp doc/physics2-legacy.tex physics2
+cp doc/physics2.pdf physics2
+cp doc/physics2-legacy.pdf physics2
+cp doc/phy2docdef.tex physics2
+COPYFILE_DISABLE=1 tar --exclude ".DS_Store" --exclude "__MACOSX" -cvzf "physics2.tar.gz" "physics2"
 rm -rf physics2
